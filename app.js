46db265d2c61a5e62aa22f9e65ded5846e03dc0a
LXie-Saintex <admin@saintex.co.uk>
2020-04-09 15:14:16 +0100

    modified time

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ hbs.registerHelper('breaklines', function(text) {
 
 hbs.registerHelper("formatDate", function(date){
 var newDate = new Date(date);
-return newDate.toLocaleString('en-GB').getTimezoneOffset();
+return newDate.toLocaleString('en-GB');
 })
 
 hbs.registerHelper("extractPath", function(path){
